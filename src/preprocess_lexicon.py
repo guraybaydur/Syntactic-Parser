eed03301c1bc908baa7b1a36bdc5ec1ee22d529b
performansur <mansuryesilbursa97@hotmail.com>
2021-01-25 00:48:00 +0300

    preprocessing and data structures completed

--- src/preprocess_lexicon.py
+++ src/preprocess_lexicon.py
@@ -5,11 +5,55 @@ def read_terminals(directory='../', file='terminal_lexicon.txt'):
     with open(directory + file, 'r', encoding='utf8') as f:
         for line in f:
             line_list = line.split() # 0 --> terminal 1 --> POS
-            if line_list[1] not in terminals.keys():
-                key = line_list[1]
-                terminals[key] = []
-            terminals[key].append(line_list[0])
+            if line_list[0] not in terminals.keys():
+                terminals[line_list[0]] = []
+            terminals[line_list[0]].append(line_list[1])
+
     return terminals
 
 
-def read_grammar
+def read_grammar(directory='../', file='grammar_rules.txt'):
+    grammar = {}
+    with open(directory+file, 'r', encoding='utf8') as f:
+        for line in f:
+            line_list = line.split('-->')
+            key = line_list[1].strip()
+            if key not in grammar.keys():
+                grammar[key] = []
+            grammar[key].append(line_list[0].strip())
+
+    return grammar
+
+
+def read_suffixes(directory='../', file='suffix_list.txt'):
+    suffixes = {}
+    with open(directory+file, 'r', encoding='utf8') as f:
+        for line in f:
+            line_list = line.split('-->')
+            key = line_list[1].strip()
+            if key not in suffixes.keys():
+                suffixes[key] = []
+            suffixes[key].append(line_list[0].strip())
+
+    return suffixes
+
+def combine_dicts(x, y, z):
+    t = x.copy()
+    t.update(y)
+    t.update(z)
+    return t
+
+def load_rules():
+    terminals = read_terminals()
+    grammar = read_grammar()
+    suffixes = read_suffixes()
+    rules = combine_dicts(terminals, grammar, suffixes)
+    return rules
+
+if __name__ == '__main__':
+    terminals = read_terminals()
+    grammar = read_grammar()
+    suffixes = read_suffixes()
+    rules = combine_dicts(terminals, grammar, suffixes)
+
+
