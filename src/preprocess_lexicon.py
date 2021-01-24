7ba1bb2c89aafefd336ccaf1aaefbe6369746ce9
performansur <mansuryesilbursa97@hotmail.com>
2021-01-24 23:06:14 +0300

    rename

--- src/preprocess_lexicon.py
+++ src/preprocess_lexicon.py
@@ -1,9 +1,15 @@
 
-terminals = {}
-with open('../terminal_lexicon.txt', 'r', encoding='utf8') as f:
-    for line in f:
-        line_list = line.split() # 0 --> terminal 1 --> POS
-        if line_list[1] not in terminals.keys():
-            key = line_list[1]
-            terminals[key] = []
-        terminals[key].append(line_list[0])
+
+def read_terminals(directory='../', file='terminal_lexicon.txt'):
+    terminals = {}
+    with open(directory + file, 'r', encoding='utf8') as f:
+        for line in f:
+            line_list = line.split() # 0 --> terminal 1 --> POS
+            if line_list[1] not in terminals.keys():
+                key = line_list[1]
+                terminals[key] = []
+            terminals[key].append(line_list[0])
+    return terminals
+
+
+def read_grammar
