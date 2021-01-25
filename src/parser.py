ebcaf0c983e566e292e63fdfcce20528fc4b51ae
performansur <mansuryesilbursa97@hotmail.com>
2021-01-25 17:41:02 +0300

    bracket form modified

--- src/parser.py
+++ src/parser.py
@@ -1,7 +1,6 @@
 from preprocess_lexicon import load_rules
 
 
-
 def parse_sentence(rules, sentence):
     terms = sentence.lower().split()
     table = [[[] for x in range(j + 1)] for j in range(len(terms))]  # generate upper triangular of the table
@@ -26,7 +25,7 @@ def parse_sentence(rules, sentence):
 
             if len(table[j][i]) == 0:  # if no combination of two elements found, insert None
                 table[j][i].append([None])
-    return table, table[len(terms)]
+    return table, table[len(terms)-1]
 
 
 #Decoding the parse, only parses the first possibility for now
@@ -41,16 +40,25 @@ def parse_sentence(rules, sentence):
 # Bu şekilde basmaya çalışıcam tree'yi
 def bracket_form_parse(parse_list):
     bracket_form = ''
-    count = 0
-    for i in parse_list:
-        bracket_form += '[' + i[0][0]
-        if count < len(parse_list) - 1:
-            temp_level = i[0][1][0].split()
-            bracket_form += ' [' + temp_level[0] + ' '
-            count += 1
-        else:
-            for j in range(count):
-                bracket_form += ']'
+    # count = 0
+    # TODO loop over second indices to include more than one parse
+    if len(parse_list) > 1: # add a loop over the second index
+        parent = parse_list[0][0][0]
+        terminal = parse_list[0][0][1][0].split()[0]
+        bracket_form += '[' + parent + ' [' + terminal + '] ' + bracket_form_parse(parse_list[1:]) + ']'
+    else:
+        parent = parse_list[0][0][0]
+        bracket_form = ' [' + parent + ']'
+
+    # for i in parse_list:
+    #     bracket_form += '[' + i[0][0]
+    #     if count < len(parse_list) - 1:
+    #         temp_level = i[0][1][0].split()
+    #         bracket_form += ' [' + temp_level[0] + ' '
+    #         count += 1
+    #     else:
+    #         for j in range(count):
+    #             bracket_form += ']'
     return bracket_form
 
 
