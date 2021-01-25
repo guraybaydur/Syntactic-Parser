f75f4ebac52f4275774983d26c79cc6aa832eceb
performansur <mansuryesilbursa97@hotmail.com>
2021-01-25 17:06:44 +0300

    parse code modified

--- src/parser.py
+++ src/parser.py
@@ -1,35 +1,34 @@
 from preprocess_lexicon import load_rules
 
 
-rules = load_rules()
-sentence = 'Ben okul a git ti m'
-terms = sentence.lower().split()
 
-table = [[[] for x in range(j+1)] for j in range(len(terms))]  # generate upper triangular of the table
-hor_ind = 1
-ver_ind = 0
+def parse_sentence(rules, sentence):
+    terms = sentence.lower().split()
+    table = [[[] for x in range(j + 1)] for j in range(len(terms))]  # generate upper triangular of the table
+    for j in range(len(terms)):
+        try:
+            table[j][j] = [rules[terms[j]]]
+        except KeyError:
+            print('Terminal not in the lexicon, can\'t parse')
+            break
+        for i in reversed(range(j)):
+            # (i,i)x(j,i+1) tüm kombinasyonlarını ara j,i'ye yaz
+            for x in table[i][i][0]:
+                for y in table[j][i+1][0]:
+                    if type(y) != list and y != None:
+                        temp_key = x + ' ' + y  # in order not to take subclauses i.e. [NP [NOUN DS]] here only take NP
+                        try:
+                            temp_list = rules[temp_key]
+                        except KeyError:
+                            continue
+                        for k in range(len(temp_list)):
+                            table[j][i].append([temp_list[k], [temp_key]])
 
-for j in range(len(terms)):
-    try:
-        table[j][j] = [rules[terms[j]]]
-    except KeyError:
-        print('Terminal not in the lexicon, can\'t parse')
-        break
-    for i in reversed(range(j)):
-        # (i,i)x(j,i+1) tüm kombinasyonlarını ara j,i'ye yaz
-        #
-        combs = []
-        for x in table[i][i]:
-            for y in table[j][i+1]:
-                temp_key = x[0] + ' ' + y[0]
-                try:
-                    temp_list = rules[temp_key]
-                except KeyError:
-                    continue
-                for k in range(len(temp_list)):
-                    table[j][i].append([temp_list[k], [temp_key]])
+            if len(table[j][i]) == 0:  # if no combination of two elements found, insert None
+                table[j][i].append([None])
+    return table, table[len(terms)]
 
-#1stSgl and 1stSglPos should be modified, one has to be removed
+
 #Decoding the parse, only parses the first possibility for now
 # z
 # # ├── c
@@ -40,17 +39,36 @@ for j in range(len(terms)):
 # # │   └── asdf
 # # └── f
 # Bu şekilde basmaya çalışıcam tree'yi
-parse_list = table[len(terms)-1]
-tree = ''
-count = 0
-for i in parse_list:
-    tree += '[' + i[0][0]
-    if count < len(parse_list) - 1:
-        temp_level = i[0][1][0].split()
-        tree +=  ' [' + temp_level[0] + ' '
-        count += 1
-    else:
-        for j in range(count):
-            tree += ']'
+def bracket_form_parse(parse_list):
+    bracket_form = ''
+    count = 0
+    for i in parse_list:
+        bracket_form += '[' + i[0][0]
+        if count < len(parse_list) - 1:
+            temp_level = i[0][1][0].split()
+            bracket_form += ' [' + temp_level[0] + ' '
+            count += 1
+        else:
+            for j in range(count):
+                bracket_form += ']'
+    return bracket_form
 
-print(tree)
+
+#from bracket to tree
+def tree_form_parse(bracket_form):
+    tree = ''
+    take = False
+    temp_str = ''
+    for i in bracket_form:
+        if i == '[':
+            take = True
+        # if take
+
+
+if __name__ == '__main__':
+    rules = load_rules()
+    sentence = 'Ben okul a git ti m'
+    sentence2 = 'Dün arkadaş ım a bir hediye al dı m'
+    parse_table, parse_list = parse_sentence(rules, sentence2)
+    bracket_form = bracket_form_parse(parse_list)
+    print(bracket_form)
